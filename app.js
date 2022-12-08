cff7586a032643307fc4835ca353f42a47f18a93
Jo Marchese <josefina.marchese@gmail.com>
2022-12-08 16:47:26 -0300

    revisión 8/12 casi final

---
+++ app.js
@@ -0,0 +1,55 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var subircvRouter = require('./routes/subircv');
+var serviciosRouter = require('./routes/servicios');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/subircv', subircvRouter);
+app.use('/servicios', serviciosRouter);
+
+app.get('/servicios', function (req, res) {
+  res.render('servicios')
+});
+
+app.get('/', function (req, res) {
+  res.render('index')
+});
+
+app.get('/subircv', function (req, res) {
+  res.render('subircv')
+});
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
