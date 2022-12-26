06f1f94cff11c8ca2008e87b240e73c009db0b23
Jo Marchese <josefina.marchese@gmail.com>
2022-12-26 14:32:33 -0300

    m5u3

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 require('dotenv').config();
 var session = require('express-session');
+var fileUpload = require('express-fileupload');
 
 var indexRouter = require('./routes/index');
 var subircvRouter = require('./routes/subircv');
@@ -44,7 +45,10 @@ secured = async (req, res, next) => {
     }
   }
 
-
+app.use(fileUpload({
+  useTempFiles: true, 
+  tempFileDir: '/tmp/'
+}));
 
 app.use('/', indexRouter);
 app.use('/subircv', subircvRouter);
--- routes/admin/novedades.js
+++ routes/admin/novedades.js
@@ -3,25 +3,53 @@ var router = express.Router();
 var novedadesModel = require('../../models/novedadesModel');
 var util = require('util');
 var novedadesModel = require('../../models/novedadesModel');
+var cloudinary = require('cloudinary').v2;
+const uploader = util.promisify(cloudinary.uploader.upload);
+const destroy = util.promisify(cloudinary.uploader.destroy);
 
 router.get('/', async function (req, res, next) {
 
-var novedades = await novedadesModel.getNovedades();
+  var novedades = await novedadesModel.getNovedades();
+  novedades = novedades.map(novedad => {
+    if (novedad.img_id) {
+      const imagen = cloudinary.image(novedad.img_id, {
+        width: 80,
+        height: 80,
+        crop: 'fill'
+      });
+      return {
+        ...novedad,
+        imagen
+      }
+    } else {
+      return {
+        ...novedad,
+        imagen: ''
+      }
+    }
+  });
+
+
+
 
   res.render('admin/novedades', {
-    layout:'admin/layout',
-    persona:req.session.nombre,
+    layout: 'admin/layout',
+    persona: req.session.nombre,
     novedades
   });
 });
 
 //eliminar por id
 router.get('/eliminar/:id', async (req, res, next) => {
-var id = req.params.id;
+  const id = req.params.id;
+  let novedad = await novedadesModel.getNovedadById(id);
+  if (novedad.img_id) {
+    await (destroy(novedad.img_id));
+  } 
 
-await novedadesModel.deleteNovedadesById(id);
-res.redirect('/admin/novedades')
-}); 
+  await novedadesModel.deleteNovedadesById(id);
+  res.redirect('/admin/novedades')
+});
 
 router.get('/agregar', (req, res, next) => {
   res.render('admin/agregar', {
@@ -31,15 +59,28 @@ router.get('/agregar', (req, res, next) => {
 
 router.post('/agregar', async (req, res, next) => {
   try {
-    if (req.body.titulo !="" && req.body.subtitulo !="" && req.body.cuerpo != "") {
-      await novedadesModel.insertNovedad(req.body);
+
+    var img_id = '';
+
+    if (req.files && Object.keys(req.files).length > 0) {
+      imagen = req.files.imagen;
+      img_id = (await uploader(imagen.tempFilePath)).public_id;
+    }
+
+    if (req.body.titulo != "" && req.body.subtitulo != "" && req.body.cuerpo != "") {
+      await novedadesModel.insertNovedad(
+        {
+          ...req.body,
+          img_id
+        });
       res.redirect('/admin/novedades')
-      } else {
-        res.render('admin/agregar', {
-          layout: 'admin/layout',
-        error: true, message: 'Todos los campos son requeridos' })
-      }
-  } catch (error) { 
+    } else {
+      res.render('admin/agregar', {
+        layout: 'admin/layout',
+        error: true, message: 'Todos los campos son requeridos'
+      })
+    }
+  } catch (error) {
     console.log(error)
     res.render('admin/agregar', {
       layout: 'admin/layout',
@@ -59,15 +100,34 @@ router.get('/modificar/:id', async (req, res, next) => {
 
 router.post('/modificar', async (req, res, next) => {
   try {
+
+    let img_id = req.body.img_original;
+    let borrar_img_vieja = false;
+    if (req.body.img_delete === "1") {
+      img_id = null;
+      borrar_img_vieja = true;
+    } else {
+      if (req.files && Object.keys(req.files).length > 0) {
+        imagen = req.files.imagen;
+        img_id = (await uploader(imagen.tempFilePath)).public_id;
+        borrar_img_vieja = true;
+      }
+    }
+    if (borrar_img_vieja && req.body.img_original) {
+      await (destroy(req.body.img_original));
+    }
+
     var obj = {
       titulo: req.body.titulo,
       subtitulo: req.body.subtitulo,
-      cuerpo: req.body.cuerpo
+      cuerpo: req.body.cuerpo,
+      img_id
     }
+
     console.log(obj)
-      await novedadesModel.modificarNovedadById(obj, req.body.id);
-      res.redirect('/admin/novedades'); 
-    } catch (error) { 
+    await novedadesModel.modificarNovedadById(obj, req.body.id);
+    res.redirect('/admin/novedades');
+  } catch (error) {
     console.log(error)
     res.render('admin/modificar', {
       layout: 'admin/layout',
@@ -76,4 +136,4 @@ router.post('/modificar', async (req, res, next) => {
   }
 });
 
- module.exports = router;
+module.exports = router;
